720c4221a568f417661d63fe34e60d21ccf73fc0
amar848301 <amar.848301@gmail.com>
2022-10-15 22:00:48 +0530

    updates

--- src/test/java/test/OAuth.java
+++ src/test/java/test/OAuth.java
@@ -8,18 +8,21 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import utility.Base;
 
-public class OAuth {
+public class OAuth extends Base {
+	
+	public static int client_id;
+	public static String access_token;
 	
 	@Test
 	public void oauthToken() throws IOException
 	{
-		Base base = new Base();
-		base.initilizeService("uid", "9988776655");
+		initilizeService("uid", "9988776655");
 		Response resp = utility.Base.request.post("/oauth");
 		String response = resp.asString();
 		
-		
 		JsonPath js = new JsonPath(response);
-		System.out.println("client_id : " +js.getInt("client_id")+ "\n" + "access_token : " +js.get("access_token"));
+		client_id = js.getInt("client_id");
+		access_token = js.get("access_token");
+		System.out.println("client_id : " +client_id+ "\n" + "access_token : " +access_token);
 	}
 }
--- src/test/java/test/Session.java
+++ src/test/java/test/Session.java
@@ -1,5 +1,25 @@
 package test;
 
-public class Session {
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import utility.Base;
+
+public class Session extends Base{
+	
+	@Test
+	public void sessionToken() throws IOException
+	{
+		base.initilizeService(client_id, "9988776655");
+		Response resp = utility.Base.request.post("/session");
+		String response = resp.asString();
+		
+		
+		JsonPath js = new JsonPath(response);
+		System.out.println("client_id : " +js.getInt("client_id")+ "\n" + "access_token : " +js.get("access_token"));
+	}
 
 }
